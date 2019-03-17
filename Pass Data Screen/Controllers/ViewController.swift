dea50db60b863d6ff28023a6ea902d83fb89e2ad
ViktorChernykh <v-cher9@yandex.ru>
2019-03-17 16:05:42 +0300

    Minor improvements

--- Pass Data Screen/Controllers/ViewController.swift
+++ Pass Data Screen/Controllers/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var loginTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    // MARK: - ... UIViewController Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         registerForKeyboardNotifications()
@@ -25,8 +26,13 @@ class ViewController: UIViewController {
         removeKeyboardNotifications()
     }
     
-    // MARK: - ... UIViewController Methods
-    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        loginTextField.text = ""
+        passwordTextField.text = ""
+    }
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let dvc = segue.destination as? SecondViewController else { return }
         dvc.loginUser = loginTextField.text
@@ -34,21 +40,6 @@ class ViewController: UIViewController {
         dvc.buttonTag = buttonTag
     }
     
-    // Remove the keyboard by the Done button
-    @IBAction func textFieldDoneEditing(sender: UITextField) {
-        if sender.placeholder! == "User Name" {
-            sender.resignFirstResponder()
-            passwordTextField.becomeFirstResponder()
-        } else {
-            sender.resignFirstResponder() // resign the first responder object
-        }
-    }
-    
-    // We remove the keyboard by touch somewhere
-    @IBAction func onTapGestureRecognized(_ sender: AnyObject) {
-        loginTextField.resignFirstResponder()
-        passwordTextField.resignFirstResponder()
-    }
     
     // register to receive notifications about the appearance / hiding of the keyboard
     func registerForKeyboardNotifications() {
@@ -62,6 +53,7 @@ class ViewController: UIViewController {
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
+    // MARK: - ... Custom Methods
     @objc func keyboardWillShow(_ notification: Notification) {
         let userInfo = notification.userInfo    // dictionary of related values / objects
         // get the keyboard size and convert to cgRectValue
@@ -80,6 +72,22 @@ class ViewController: UIViewController {
     }
     
     // MARK: - ... @IBAction
+    // Remove the keyboard by the Done button
+    @IBAction func textFieldDoneEditing(sender: UITextField) {
+        if sender.placeholder == "User Name" {
+            sender.resignFirstResponder()
+            passwordTextField.becomeFirstResponder()
+        } else {
+            sender.resignFirstResponder() // resign the first responder object
+        }
+    }
+    
+    // We remove the keyboard by touch somewhere
+    @IBAction func onTapGestureRecognized(_ sender: AnyObject) {
+        loginTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+    }
+    
     @IBAction func loginPressed(_ sender: UIButton) {
         buttonTag = sender.tag
         performSegue(withIdentifier: "SecondSegue", sender: nil)
